7065ba2c1f453963159e4db6c66f1f4a31b0b8ef
Salvadortarrio <salvador.tarrio@campusdual.com>
2025-02-08 18:06:18 +0100

    retoques en modulos fijos

--- fixed_resources/ecr/main.tf
+++ fixed_resources/ecr/main.tf
@@ -40,11 +40,3 @@ resource "aws_ecr_repository_policy" "example" {
   policy     = data.aws_iam_policy_document.example.json
 }
 
-terraform {
-  backend "s3" {
-    bucket = "proyecto-devops-grupo-dos"          # Nombre de tu bucket S3
-    key    = "ecr/terraform.tfstate"           # Ruta y nombre del archivo de estado dentro del bucket
-    region = "eu-west-2"                           # Región donde está tu bucket S3
-    encrypt = true                                   # Habilita el cifrado en el bucket
-  }
-}
--- fixed_resources/ecr/variables.tf
+++ fixed_resources/ecr/variables.tf
@@ -1,11 +1,13 @@
 variable "repository_name" {
   description = "ECR repository name"
   type        = string
+  default="repositorio-grupo-dos"
 }
 
 
 variable "aws_region" {
   description = "La región de AWS donde se desplegarán los recursos"
   type        = string
+  default="eu-west-2"
   
 }
