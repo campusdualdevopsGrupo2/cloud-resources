ac56ebcd1fb90b513bc075b866850bbd846c2883
Jorge382 <jorge.vidal@campusdual.com>
2025-02-09 22:16:09 +0100

    Modificaciones para clase 8 modulo 2

--- modulo2/clase8/wp_irlanda/main.tf
+++ modulo2/clase8/wp_irlanda/main.tf
@@ -200,11 +200,4 @@ resource "null_resource" "ansible_provision" {
 }
 
 
-terraform {
-  backend "s3" {
-    bucket  = "proyecto-devops-grupo-dos"          # Nombre de tu bucket S3
-    key     = "ec2-práctica-modulo2-clase4/terraform.tfstate"  # Ruta y nombre del archivo de estado dentro del bucket
-    region  = "eu-west-2"                           # Región donde está tu bucket S3
-    encrypt = true                                  # Habilita el cifrado en el bucket
-  }
-}
+
