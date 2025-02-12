e2329996ea1fc46dd0b3d54132bc5aaecf48c0db
Salvadortarrio <salvador.tarrio@campusdual.com>
2025-02-12 19:43:43 +0100

    modulo sg corregido : no se incluia caso dependencia entre sgs.

--- g2-cdt-prod-sg/main.tf
+++ g2-cdt-prod-sg/main.tf
@@ -10,6 +10,7 @@ resource "aws_security_group" "this" {
       to_port     = ingress.value.to_port
       protocol    = ingress.value.protocol
       cidr_blocks  = ingress.value.cidr_blocks
+      security_groups = ingress.value.security_groups
     }
   }
 
@@ -21,6 +22,7 @@ resource "aws_security_group" "this" {
       to_port     = egress.value.to_port
       protocol    = egress.value.protocol
       cidr_blocks  = egress.value.cidr_blocks
+      security_groups = egress.value.security_groups
     }
   }
 }
--- g2-cdt-prod-sg/variables.tf
+++ g2-cdt-prod-sg/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "La región de AWS donde se creará el grupo de seguridad."
   type        = string
-  default     = "us-east-1"
+  default     = "eu-west-2"
 }
 
 variable "security_group_name" {
@@ -14,24 +14,22 @@ variable "vpc_id" {
   type        = string
 }
 
-variable "ingress_rules" {
-  description = "Lista de reglas de entrada para el grupo de seguridad."
-  type        = list(object({
-    from_port   = number
-    to_port     = number
-    protocol    = string
-    cidr_blocks  = list(string)
+variable "egress_rules" {
+  type = list(object({
+    from_port       = number
+    to_port         = number
+    protocol        = string
+    cidr_blocks     = list(string)
+    security_groups = list(string)  # Ahora puedes pasar una lista de IDs de SG
   }))
-  default = []
 }
 
-variable "egress_rules" {
-  description = "Lista de reglas de salida para el grupo de seguridad."
-  type        = list(object({
-    from_port   = number
-    to_port     = number
-    protocol    = string
-    cidr_blocks  = list(string)
+variable "ingress_rules" {
+  type = list(object({
+    from_port       = number
+    to_port         = number
+    protocol        = string
+    cidr_blocks     = list(string)
+    security_groups = list(string)  # Ahora puedes pasar una lista de IDs de SG
   }))
-  default = []
 }
