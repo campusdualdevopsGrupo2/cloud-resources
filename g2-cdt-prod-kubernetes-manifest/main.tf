25e66abbe946dff3ac28cf4a04f20d306e5bc174
Jorge382 <jorge.vidal@campusdual.com>
2025-02-12 21:28:18 +0100

    Modificaciones a modulo Kubernetes manifest

--- g2-cdt-prod-kubernetes-manifest/main.tf
+++ g2-cdt-prod-kubernetes-manifest/main.tf
@@ -1,15 +1,31 @@
-data "template_file" "generic_manifest" {
-  template = file(var.manifest_yaml)
-
-  vars = {
-    name = var.manifest_name
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.11.0"
+    }
   }
 }
 
+# Define the Kubernetes provider for context1 with a static alias.
+provider "kubernetes" {
+  config_path    = var.kubeconfig_path
+  config_context = var.kubeconfig_context
+}
 
+# Use a data block to load and template your manifest file.
+data "template_file" "generic_manifest" {
+  template = file(var.manifest_yaml)  # The manifest file (e.g., manifest.yaml)
+  vars = {
+    name = var.manifest_name         # This variable can be substituted inside the YAML file.
+  }
+}
+
+# Create a Kubernetes manifest resource using the templated YAML.
 resource "kubernetes_manifest" "generic_deployment" {
   manifest = yamldecode(data.template_file.generic_manifest.rendered)
 
-  # Configura otros parámetros si es necesario, como el contexto del clúster
-  provider = kubernetes.${var.kubernetes_context} # Asegúrate de tener un proveedor adecuado
+  # Reference the statically defined provider alias.
+  provider = kubernetes
 }
