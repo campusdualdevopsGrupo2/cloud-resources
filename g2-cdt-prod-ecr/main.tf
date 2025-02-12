e4a9c09013b56538ed47fa1fb51b63f96e526465
Jorge382 <jorge.vidal@campusdual.com>
2025-02-12 19:45:47 +0100

    Cambios en modulo ecr

--- g2-cdt-prod-ecr/main.tf
+++ g2-cdt-prod-ecr/main.tf
@@ -1,42 +1,25 @@
 resource "aws_ecr_repository" "repository" {
   name = var.repository_name
 
-  tags = {
-    Project = "Repositorio-grupo-dos"
-  }
+  tags = var.tags
 }
 
-data "aws_iam_policy_document" "example" {
+data "aws_iam_policy_document" "ecr_policy" {
   statement {
-    sid    = "new policy"
+    sid    = var.policy_sid
     effect = "Allow"
 
     principals {
       type        = "AWS"
-      identifiers = ["arn:aws:sts::248189943700:assumed-role/AWSReservedSSO_EKS-alumnos_a4561514b13725b0/jorge.vidal@campusdual.com"]
+      identifiers = var.policy_principals
     }
 
-    actions = [
-      "ecr:GetDownloadUrlForLayer",
-      "ecr:BatchGetImage",
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:PutImage",
-      "ecr:InitiateLayerUpload",
-      "ecr:UploadLayerPart",
-      "ecr:CompleteLayerUpload",
-      "ecr:DescribeRepositories",
-      "ecr:GetRepositoryPolicy",
-      "ecr:ListImages",
-      "ecr:DeleteRepository",
-      "ecr:BatchDeleteImage",
-      "ecr:SetRepositoryPolicy",
-      "ecr:DeleteRepositoryPolicy",
-    ]
+    actions = var.policy_actions
   }
 }
 
-resource "aws_ecr_repository_policy" "example" {
+resource "aws_ecr_repository_policy" "repository_policy" {
   repository = aws_ecr_repository.repository.name
-  policy     = data.aws_iam_policy_document.example.json
+  policy     = data.aws_iam_policy_document.ecr_policy.json
 }
 
--- g2-cdt-prod-ecr/variables.tf
+++ g2-cdt-prod-ecr/variables.tf
@@ -1,13 +1,48 @@
 variable "repository_name" {
-  description = "ECR repository name"
+  description = "Nombre del repositorio ECR"
   type        = string
-  default="repositorio-grupo-dos"
 }
 
-
-variable "aws_region" {
-  description = "La región de AWS donde se desplegarán los recursos"
-  type        = string
-  default="eu-west-2"
-  
+variable "tags" {
+  description = "Etiquetas para el repositorio ECR"
+  type        = map(string)
+  default = {
+    Project = "Repositorio-grupo-dos"
+  }
 }
+
+variable "policy_sid" {
+  description = "SID para la política de ECR"
+  type        = string
+  default     = "new policy"
+}
+
+variable "policy_principals" {
+  description = "Lista de identificadores (ARNs) para el principal de la política"
+  type        = list(string)
+  default     = [
+    "arn:aws:sts::248189943700:assumed-role/AWSReservedSSO_EKS-alumnos_a4561514b13725b0/jorge.vidal@campusdual.com"
+  ]
+}
+
+variable "policy_actions" {
+  description = "Lista de acciones permitidas en la política de ECR"
+  type        = list(string)
+  default     = [
+    "ecr:GetDownloadUrlForLayer",
+    "ecr:BatchGetImage",
+    "ecr:BatchCheckLayerAvailability",
+    "ecr:PutImage",
+    "ecr:InitiateLayerUpload",
+    "ecr:UploadLayerPart",
+    "ecr:CompleteLayerUpload",
+    "ecr:DescribeRepositories",
+    "ecr:GetRepositoryPolicy",
+    "ecr:ListImages",
+    "ecr:DeleteRepository",
+    "ecr:BatchDeleteImage",
+    "ecr:SetRepositoryPolicy",
+    "ecr:DeleteRepositoryPolicy"
+  ]
+}
+
