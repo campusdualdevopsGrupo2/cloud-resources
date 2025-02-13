8f827a0fe7f8b4052641cf3f8d6e782d4adebbff
Salvadortarrio <salvador.tarrio@campusdual.com>
2025-02-13 17:27:23 +0100

    no dependency

--- g2-cdt-prod-data-specific1/main.tf
+++ g2-cdt-prod-data-specific1/main.tf
@@ -1,7 +1,6 @@
 data "external" "get_dns" {
   # En caso de usar Python, usa este comando:
    program = ["python3", "get_dns.py", "chat-app-service"]
-   depends_on=[kubernetes_manifest.nginx_deployment_service]
 }
 
 output "dns_value" {
